eeb48754c293bec115e95c06d3cbd94d311807ca
Metricadia Research LLC <noirsoulkin@gmail.com>
2026-04-19 03:51:15 -0700

    Sync index.ts via GitHub API

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 import { logger } from "./lib/logger";
-import { seedIfEmpty, insertNewArticles, insertFoundersPenArticles, updateNativeArticles, syncImprovedArticles, applyPremiumFlags, applyStaffPickFlags, applyCategoryOverrides, applyCaseNumberRenames } from "./seed";
+import { seedIfEmpty, insertNewArticles, insertFoundersPenArticles, updateNativeArticles, syncImprovedArticles, applyPremiumFlags, applyStaffPickFlags, applyCategoryOverrides, applyCaseNumberRenames, applyContentLocks } from "./seed";
 
 const rawPort = process.env["PORT"];
 
@@ -33,5 +33,6 @@ app.listen(port, (err) => {
     .then(() => applyCategoryOverrides())
     .then(() => applyPremiumFlags())
     .then(() => applyStaffPickFlags())
+    .then(() => applyContentLocks())
     .catch((e) => logger.error({ err: e }, "Seed failed"));
 });
